14a990980e6f44d00b2ede0d8d5b36fef1403805
FelixLevesqueCM <2368669@cmontmorency.qc.ca>
2025-09-02 10:26:47 -0400

    pixel_50%

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,28 @@
 
 #define MA_BROCHE_ANGLE 32
 
+#include <FastLED.h>
+
+CRGB monPixel;
+
 void setup() {
   Serial.begin(115200);
+  FastLED.addLeds<WS2812,27,GRB>(&monPixel,1);
 }
 
 void loop() {
   int maLectureAngle = analogRead(MA_BROCHE_ANGLE);
   Serial.println(maLectureAngle);
+
+  if (maLectureAngle >= 2000)
+  {
+    monPixel = CRGB(255,0,0);
+    FastLED.show();
+  } if (maLectureAngle < 2000)
+  {
+    monPixel = CRGB(0,0,0);
+    FastLED.show();
+  }
+
   delay(100);
 }
